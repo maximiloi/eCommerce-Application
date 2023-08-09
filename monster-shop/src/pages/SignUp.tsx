ddd88ced23431da51cbe04987d6f8fabe26890aa
maximiloi <maximiloi@gmail.com>
2023-08-09 12:50:45 +0300

    feat: add signUp component

--- monster-shop/src/pages/SignUp.tsx
+++ monster-shop/src/pages/SignUp.tsx
@@ -1,8 +1,11 @@
+import FormSignUp from '../components/FormSignUp/FormSignUp';
+
 function SignUp() {
   return (
     <div className="register">
       <div className="register__container">
         <h2>Sign Up</h2>
+        <FormSignUp />
       </div>
     </div>
   );
