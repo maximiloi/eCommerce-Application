2908f62131a80ad9d7169defb71a2920d5ccb2a7
maximiloi <maximiloi@gmail.com>
2023-08-10 13:45:39 +0300

    feat: add validation Sign Up form

--- monster-shop/src/components/FormSignUp/FormSignUp.tsx
+++ monster-shop/src/components/FormSignUp/FormSignUp.tsx
@@ -7,19 +7,23 @@ type FormValues = {
   firstName: string;
   lastName: string;
   password: string;
-  repeatPassword: string;
+  confirmPassword: string;
 };
 
 export default function FormSignUp() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>();
+    formState: { errors, isValid },
+    reset,
+    watch,
+    getValues,
+  } = useForm<FormValues>({ mode: 'onBlur' });
 
-  const onSubmit = (data: FormValues) => console.log(data);
-
-  console.log(errors);
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="form">
@@ -27,32 +31,64 @@ export default function FormSignUp() {
         type="text"
         placeholder="E-mail"
         {...register('eMail', {
-          required: true,
-          pattern: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+          required: 'Enter your e-mail, required field',
+          pattern: {
+            value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i,
+            message: 'Enter e-mail',
+          },
         })}
       />
+      {errors?.eMail && (
+        <span>{errors?.eMail.message || 'Required field'}</span>
+      )}
       <input
         type="text"
         placeholder="First Name"
-        {...register('firstName', { required: true })}
+        {...register('firstName', {
+          required: 'Enter your "First Name", required field',
+        })}
       />
+      {errors?.firstName && (
+        <span>{errors?.firstName.message || 'Required field'}</span>
+      )}
       <input
         type="text"
         placeholder="Last Name"
-        {...register('lastName', { required: false })}
+        {...register('lastName', {
+          required: 'Enter your "Last Name", required field',
+        })}
       />
+      {errors?.lastName && (
+        <span>{errors?.lastName.message || 'Required field'}</span>
+      )}
       <input
         type="password"
         placeholder="Password"
-        {...register('password', { required: true })}
+        {...register('password', {
+          required: 'Password is required',
+          minLength: {
+            value: 6,
+            message: 'Password must have at least 6 characters',
+          },
+        })}
       />
+      {errors?.password && (
+        <span>
+          {errors?.password.message || 'Password must be at least 6 characters'}
+        </span>
+      )}
       <input
         type="password"
-        placeholder="Repeat Password"
-        {...register('repeatPassword', { required: true })}
+        placeholder="Confirm Password"
+        {...register('confirmPassword', {
+          required: 'Confirm Password is required',
+        })}
       />
-
-      <input type="submit" />
+      {watch('confirmPassword') !== watch('password') &&
+      getValues('confirmPassword') ? (
+        <span>Confirm Password not match</span>
+      ) : null}
+      <input type="submit" disabled={!isValid} />
     </form>
   );
 }
