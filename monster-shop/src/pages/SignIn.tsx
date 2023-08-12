67f47b76269663199223e9b137ad208c2a90a02c
maximiloi <maximiloi@gmail.com>
2023-08-12 19:09:32 +0300

    feat: add styled sign in page

--- monster-shop/src/pages/SignIn.tsx
+++ monster-shop/src/pages/SignIn.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import FormSignIn from '../components/FormSignIn/FormSignIn';
 
+import '../sass/pages/_SignIn.scss';
+
 function SignIn() {
   return (
-    <div className="login">
-      <div className="login__container">
+    <div className="sign-in">
+      <div className="sign-in__container">
         <h2>Sign In</h2>
         <FormSignIn />
         <NavLink to="/auth/register">No Account, Register</NavLink>
