83e2460feac8109ea084265bebddb09008502f51
GruntovGD <gruntovgd@gmail.com>
2023-08-16 22:44:02 +0300

    feat: add redirection to main for authorized user

--- monster-shop/src/App.tsx
+++ monster-shop/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -6,6 +6,15 @@ import Page404 from './pages/Page404';
 import Header from './components/Header/Header';
 
 import './App.scss';
+import User from './api/user';
+
+function RedirectSignIn() {
+  return User.created ? <Navigate to="/" /> : <SignIn />;
+}
+
+function RedirectSignUp() {
+  return User.created ? <Navigate to="/" /> : <SignUp />;
+}
 
 function App() {
   return (
@@ -15,8 +24,8 @@ function App() {
         <div className="main__container">
           <Routes>
             <Route path="/" element={<MainPage />} />
-            <Route path="/auth" element={<SignIn />} />
-            <Route path="register" element={<SignUp />} />
+            <Route path="/auth" element={<RedirectSignIn />} />
+            <Route path="register" element={<RedirectSignUp />} />
             <Route path="/*" element={<Page404 />} />
           </Routes>
         </div>
