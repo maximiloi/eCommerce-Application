884412f25ea2fffa4d1c4a33b525dd6e2beb56a2
maximiloi <maximiloi@gmail.com>
2023-08-19 20:36:52 +0300

    faet: add check more 150 age

--- monster-shop/src/helper/validateDateBirth.ts
+++ monster-shop/src/helper/validateDateBirth.ts
@@ -2,6 +2,7 @@ const validateDateBirth: (value: string | null) => string | true = (
   value: string | null
 ): string | true => {
   const ageConsent = 13;
+  const bigAge = 150;
 
   if (value !== null) {
     const today = new Date();
@@ -10,6 +11,10 @@ const validateDateBirth: (value: string | null) => string | true = (
 
     if (age < ageConsent) {
       return `Your age must be over ${ageConsent} years old`;
+    }
+
+    if (age > 150) {
+      return `You can't be more than ${bigAge} years old`;
     }
   }
 
