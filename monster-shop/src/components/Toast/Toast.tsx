ecf2d1a3956e6319702911c2f148cb211382a3b3
maximiloi <maximiloi@gmail.com>
2023-08-21 00:23:59 +0300

    feat: add default promise

---
+++ monster-shop/src/components/Toast/Toast.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function App() {
+  const myPromise = new Promise((resolve) =>
+    // eslint-disable-next-line no-promise-executor-return
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setTimeout(() => resolve(json), 3000))
+  );
+
+  useEffect(() => {
+    toast.promise(myPromise, {
+      pending: 'Promise is pending',
+      success: 'Promise  Loaded',
+      error: 'error',
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="App">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </div>
+  );
+}
