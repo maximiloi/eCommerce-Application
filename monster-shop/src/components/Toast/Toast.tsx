524e1530282edf3d94fa9eefc01c125561054a0a
maximiloi <maximiloi@gmail.com>
2023-08-21 10:11:48 +0300

    feat: create func toastify and styled

--- monster-shop/src/components/Toast/Toast.tsx
+++ monster-shop/src/components/Toast/Toast.tsx
@@ -1,29 +1,12 @@
-import { useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Toast() {
-  const myPromise = new Promise((resolve) =>
-    // eslint-disable-next-line no-promise-executor-return
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => response.json())
-      .then((json) => setTimeout(() => resolve(json), 3000))
-  );
-
-  useEffect(() => {
-    toast.promise(myPromise, {
-      pending: 'Promise is pending',
-      success: 'Promise  Loaded',
-      error: 'error',
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className="Toast">
       <ToastContainer
         position="bottom-center"
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
