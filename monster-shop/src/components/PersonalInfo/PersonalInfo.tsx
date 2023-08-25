d7d6263427c9711bda5c0d9c1791cccb29686522
maximiloi <maximiloi@gmail.com>
2023-08-25 13:34:19 +0300

    feat: draft comp personal info

---
+++ monster-shop/src/components/PersonalInfo/PersonalInfo.tsx
@@ -0,0 +1,87 @@
+import { useForm } from 'react-hook-form';
+import { CustomerSignin } from '@commercetools/platform-sdk';
+import { Grid, Paper, styled } from '@mui/material';
+import dayjs from 'dayjs';
+import TextFieldInput from '../Inputs/TextFieldInput';
+// import ColoredBtn from '../ColoredBtn/ColoredBtn';
+import User from '../../api/user';
+import validatePassword from '../../helper/validatePassword';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function PersonalInfo() {
+  const { data } = User;
+  console.log(
+    'data: ',
+    data?.firstName,
+    data?.lastName,
+    data?.dateOfBirth,
+    data?.email
+  );
+
+  const { control } = useForm<CustomerSignin>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onChange',
+  });
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} md={8}>
+        <Item>
+          <TextFieldInput
+            name="firstName"
+            control={control}
+            label="First Name"
+            defaultValue={data?.firstName}
+          />
+          <TextFieldInput
+            name="lastName"
+            control={control}
+            label="Last Name"
+            defaultValue={data?.lastName}
+          />
+          <TextFieldInput
+            name="dateOfBirth"
+            control={control}
+            label="Date of birth"
+            defaultValue={dayjs(data?.dateOfBirth).format('DD/MM/YYYY')}
+          />
+        </Item>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Item>
+          <TextFieldInput
+            name="email"
+            control={control}
+            label="E-mail"
+            defaultValue={data?.email}
+          />
+          <TextFieldInput
+            name="password"
+            control={control}
+            label="Password"
+            rules={{
+              required: 'Enter your password, required field',
+              minLength: {
+                value: 8,
+                message: 'Password must have at least 8 characters',
+              },
+              validate: validatePassword,
+            }}
+          />
+        </Item>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default PersonalInfo;
--- monster-shop/src/pages/Profile.tsx
+++ monster-shop/src/pages/Profile.tsx
@@ -1,18 +1,16 @@
 import { Box, Grid } from '@mui/material';
-import User from '../api/user';
+import PersonalInfo from '../components/PersonalInfo/PersonalInfo';
 
 import '../sass/pages/Profile.scss';
 
 function Profile() {
-  const { data } = User;
-  console.log('email: ', data?.email);
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <h2>User Profile</h2>
       <Grid container spacing={2} className="profile__wrapper">
         <Grid item>
           <h4>Personal info</h4>
+          <PersonalInfo />
           <h4>Shipping address</h4>
           <h4>Billing address</h4>
         </Grid>
