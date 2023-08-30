30c7bd5f7f157089bea8a4ed0eaf7e86e9fd8173
GruntovGD <gruntovgd@gmail.com>
2023-08-30 20:47:05 +0300

    refactor: update login/signup requests

---
+++ monster-shop/src/api/requests.ts
@@ -0,0 +1,45 @@
+import { CustomerSignin, MyCustomerDraft } from '@commercetools/platform-sdk';
+import toastify from '../helper/toastify';
+import User from './user';
+
+export async function login(customerSignin: CustomerSignin) {
+  return new Promise((resolve) => {
+    User.getApi()
+      .me()
+      .login()
+      .post({
+        body: customerSignin,
+      })
+      .execute()
+      .then((response) => {
+        User.signin(response.body.customer);
+        toastify(`Hello, ${User.data?.firstName}`, 'success');
+        resolve(response);
+      })
+      .catch((error) => {
+        console.error('Error login user:', error);
+        toastify(error.message, 'error');
+      });
+  });
+}
+
+export async function signup(myCustomerDraft: MyCustomerDraft) {
+  return new Promise((resolve) => {
+    User.getApi()
+      .me()
+      .signup()
+      .post({
+        body: myCustomerDraft,
+      })
+      .execute()
+      .then((response) => {
+        User.signin(response.body.customer);
+        toastify(`Hello, ${User.data?.firstName}`, 'success');
+        resolve(response);
+      })
+      .catch((error) => {
+        console.error('Error registering user:', error);
+        toastify(error.message, 'error');
+      });
+  });
+}
