481bfc594daf4c29a48e855fe790dca1b9cc7e09
tat.bandelikova <tat.bandelikova@gmail.com>
2023-09-03 12:56:13 +0300

    feat: breadcrumps var with navLink

--- monster-shop/src/pages/ProductPage.tsx
+++ monster-shop/src/pages/ProductPage.tsx
@@ -1,12 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import { Box, Breadcrumbs } from '@mui/material';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 
 function ProductPage() {
   const { productId } = useParams();
   return (
-    <div>
-      <h2>Product Detail Page</h2>
-      <em>id: {productId}</em>
-    </div>
+    <Box className="product" sx={{ display: 'flex', flexWrap: 'nowrap' }}>
+      <Box className="product__content">
+        <Breadcrumbs className="breadcrumb" aria-label="breadcrumb">
+          <NavLink className="breadcrumb__item" to="/catalog">
+            <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+            All Monsters
+          </NavLink>
+          <p className="breadcrumb__item breadcrumb__item_active">
+            Product Details
+          </p>
+        </Breadcrumbs>
+        <em>id: {productId}</em>
+      </Box>
+    </Box>
   );
 }
 
