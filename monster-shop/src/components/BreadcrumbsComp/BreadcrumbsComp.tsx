f14611c8751748a0165919d6efadbc24806a2c4e
maximiloi <maximiloi@gmail.com>
2023-09-04 12:06:00 +0300

    reafctor: fetchProductData useCallback

--- monster-shop/src/components/BreadcrumbsComp/BreadcrumbsComp.tsx
+++ monster-shop/src/components/BreadcrumbsComp/BreadcrumbsComp.tsx
@@ -1,26 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 import { Box, Breadcrumbs } from '@mui/material';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import { ProductProjection } from '@commercetools/platform-sdk';
-import { getProductId } from '../../api/requests';
+import { getProductId, getCategoryId } from '../../api/requests';
 
 function BreadcrumbsComp() {
   const { productId } = useParams();
   const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
 
-  async function fetchProductData(id: string) {
+  async function fetchNameCategory(id: string) {
     try {
-      const productResponce = (await getProductId(id)) as ProductProjection;
-      setTitle(productResponce.name.en);
+      const categoryResponce = (await getCategoryId(id)) as ProductProjection;
+      setCategory(categoryResponce.name.en);
     } catch (err) {
       console.log(err);
     }
   }
 
+  const fetchProductData = useCallback(async (id: string) => {
+    try {
+      const productResponce = (await getProductId(id)) as ProductProjection;
+      setTitle(productResponce.name.en);
+      await fetchNameCategory(productResponce.categories[0].id);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   useEffect(() => {
     fetchProductData(productId as string);
-  }, [productId]);
+  }, [productId, fetchProductData]);
 
   return (
     <Box
@@ -33,6 +44,9 @@ function BreadcrumbsComp() {
             <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" />
             All Monsters
           </NavLink>
+          {/* <NavLink className="breadcrumb__item" to="/catalog"> */}
+          <p className="breadcrumb__item">{category}</p>
+          {/* </NavLink> */}
           <p className="breadcrumb__item breadcrumb__item_active">{title}</p>
         </Breadcrumbs>
       </Box>
