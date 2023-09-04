5a9d1db08c6fd1e3ddbf28100dc94b3cf1b4b32e
maximiloi <maximiloi@gmail.com>
2023-09-04 11:24:23 +0300

    feat: add name product

--- monster-shop/src/components/BreadcrumbsComp/BreadcrumbsComp.tsx
+++ monster-shop/src/components/BreadcrumbsComp/BreadcrumbsComp.tsx
@@ -1,19 +1,39 @@
-import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
 import { Box, Breadcrumbs } from '@mui/material';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import { ProductProjection } from '@commercetools/platform-sdk';
+import { getProductId } from '../../api/requests';
 
 function BreadcrumbsComp() {
+  const { productId } = useParams();
+  const [title, setTitle] = useState('');
+
+  async function fetchProductData(id: string) {
+    try {
+      const productResponce = (await getProductId(id)) as ProductProjection;
+      setTitle(productResponce.name.en);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchProductData(productId as string);
+  }, [productId]);
+
   return (
-    <Box className="product" sx={{ display: 'flex', flexWrap: 'nowrap' }}>
+    <Box
+      className="product"
+      sx={{ display: 'flex', flexWrap: 'nowrap', pb: 2 }}
+    >
       <Box className="product__content">
         <Breadcrumbs className="breadcrumb" aria-label="breadcrumb">
           <NavLink className="breadcrumb__item" to="/catalog">
             <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" />
             All Monsters
           </NavLink>
-          <p className="breadcrumb__item breadcrumb__item_active">
-            Product Details
-          </p>
+          <p className="breadcrumb__item breadcrumb__item_active">{title}</p>
         </Breadcrumbs>
       </Box>
     </Box>
