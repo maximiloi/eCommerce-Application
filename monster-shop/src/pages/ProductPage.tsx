b68bfdd1bddb2c1fbe622e4d6da904b28ef7caa1
maximiloi <maximiloi@gmail.com>
2023-09-04 10:44:23 +0300

    feat: add empty div

--- monster-shop/src/pages/ProductPage.tsx
+++ monster-shop/src/pages/ProductPage.tsx
@@ -1,27 +1,29 @@
-import { NavLink, useParams } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Box, Breadcrumbs } from '@mui/material';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import ProductPageCard from '../components/ProductPage/ProductPageComp';
 
 function ProductPage() {
   return (
-    <Box className="product" sx={{ display: 'flex', flexWrap: 'nowrap' }}>
-      <Box className="product__content">
-        <Breadcrumbs className="breadcrumb" aria-label="breadcrumb">
-          <NavLink className="breadcrumb__item" to="/catalog">
-            <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" />
-            All Monsters
-          </NavLink>
-          <p className="breadcrumb__item breadcrumb__item_active">
-            Product Details
-          </p>
-        </Breadcrumbs>
-        <em>id: {productId}</em>
+    <>
+      <Box className="product" sx={{ display: 'flex', flexWrap: 'nowrap' }}>
+        <Box className="product__content">
+          <Breadcrumbs className="breadcrumb" aria-label="breadcrumb">
+            <NavLink className="breadcrumb__item" to="/catalog">
+              <AutoStoriesIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+              All Monsters
+            </NavLink>
+            <p className="breadcrumb__item breadcrumb__item_active">
+              Product Details
+            </p>
+          </Breadcrumbs>
+          <em>id</em>
+        </Box>
       </Box>
-    </Box>
-    <div>
-      <ProductPageCard />
-    </div>
+      <div>
+        <ProductPageCard />
+      </div>
+    </>
   );
 }
 
