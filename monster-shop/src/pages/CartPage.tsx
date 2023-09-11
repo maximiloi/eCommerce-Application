4441d63b0246165b80e80cb94cc8b7acdd9e5484
tat.bandelikova <tat.bandelikova@gmail.com>
2023-09-11 20:41:01 +0300

    feat: add cart components

--- monster-shop/src/pages/CartPage.tsx
+++ monster-shop/src/pages/CartPage.tsx
@@ -1,24 +1,75 @@
 import { useState } from 'react';
-import { Box } from '@mui/material';
-import '../sass/pages/_cartPage.scss';
 import { NavLink } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+} from '@mui/material';
+import PromoCodeBar from '../components/PromoCodeBar/PromoCodeBar';
+import ColoredBtn from '../components/ColoredBtn/ColoredBtn';
+import '../sass/pages/_cartPage.scss';
 
 function CartPage() {
-  const [isEmpty, setIsEmpty] = useState(true);
+  const [isEmpty] = useState(true); // setIsEmpty
+  const [promoCode, setPromoCode] = useState<string>('');
 
+  console.log(promoCode);
   return (
-    <Box className="cart" sx={{ display: 'flex' }}>
+    <Box
+      className="cart"
+      sx={{ display: 'flex', flexDirection: { xs: 'column', sm: 'row' } }}
+    >
       <Box
         className="cart__content"
         sx={{ flexGrow: 1, width: { sm: 'calc(100% - 200px)' }, p: 1 }}
       >
         {isEmpty && (
-          <NavLink to="/catalog">
+          <NavLink to="/catalog" className="cart__link">
             Nothing here yet... Return to the catalog!
           </NavLink>
         )}
       </Box>
-      <Box component="aside" className="cart__aside" sx={{ flexGrow: 0 }} />
+      <Box component="aside" className="cart__aside" sx={{ flexGrow: 1 }}>
+        <h4 className="cart__subtitle">Enter promo code</h4>
+        <PromoCodeBar setPromoCode={setPromoCode} />
+        <Table className="cart__calculate" size="small">
+          <TableBody>
+            <TableRow>
+              <TableCell>Shipping cost</TableCell>
+              <TableCell align="right">0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Discount</TableCell>
+              <TableCell align="right">-0</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Tax</TableCell>
+              <TableCell align="right">0</TableCell>
+            </TableRow>
+            <TableRow className="total">
+              <TableCell>Total</TableCell>
+              <TableCell align="right">0</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        <Box className="cart__action">
+          <Button className="btn order" fullWidth type="button">
+            Order monsters
+          </Button>
+          <span className="divider">Or</span>
+          <ColoredBtn
+            type="button"
+            variant="contained"
+            fullWidth
+            disabled={false}
+          >
+            Clear Shopping Cart
+          </ColoredBtn>
+        </Box>
+      </Box>
     </Box>
   );
 }
