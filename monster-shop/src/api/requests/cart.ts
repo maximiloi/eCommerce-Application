18f6506d13ecdc27f7d3f9ae90cdf0d2028981b0
GruntovGD <gruntovgd@gmail.com>
2023-09-12 22:29:34 +0300

    feat: cart item add request

--- monster-shop/src/api/requests/cart.ts
+++ monster-shop/src/api/requests/cart.ts
@@ -1,7 +1,8 @@
+import { Cart } from '@commercetools/platform-sdk';
 import toastify from '../../helper/toastify';
 import User from '../user';
 
-export function getCart() {
+export function getCarts() {
   return new Promise((resolve) => {
     User.getApi()
       .me()
@@ -9,11 +10,11 @@ export function getCart() {
       .get()
       .execute()
       .then((response) => {
-        console.log(response.body);
-        resolve(response.body);
+        console.log(response.body.results);
+        resolve(response.body.results);
       })
       .catch((error) => {
-        console.error('Error login user:', error);
+        console.error('Error getting carts:', error);
         toastify(error.message, 'error');
       });
   });
@@ -32,10 +33,38 @@ export function createCart() {
       .execute()
       .then((response) => {
         console.log(response.body);
+        resolve(response.body);
+      })
+      .catch((error) => {
+        console.error('Error creating cart:', error);
+        toastify(error.message, 'error');
+      });
+  });
+}
+
+export async function cartAddItem(productId: string) {
+  const carts = (await getCarts()) as Cart[];
+  const cart = carts[carts.length - 1] || ((await createCart()) as Cart);
+  console.log(cart);
+  const { version, id } = cart;
+  return new Promise((resolve) => {
+    User.getApi()
+      .me()
+      .carts()
+      .withId({ ID: id })
+      .post({
+        body: {
+          version,
+          actions: [{ action: 'addLineItem', productId }],
+        },
+      })
+      .execute()
+      .then((response) => {
+        console.log(response.body);
         resolve(response);
       })
       .catch((error) => {
-        console.error('Error login user:', error);
+        console.error('Error:', error);
         toastify(error.message, 'error');
       });
   });
